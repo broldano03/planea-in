00538dbc02fdba39999b8cc2e2397686a72a49fe
yusshu <andreroldan.me@gmail.com>
2024-11-12 23:11:14 -0500

    feat: make it necessary to be logged in to access / and /tablero-kanban

--- src/components/main/Template.jsx
+++ src/components/main/Template.jsx
@@ -1,7 +1,42 @@
-import { Outlet } from "react-router-dom"
+import {Outlet, useNavigate} from "react-router-dom"
 import Header from "../cmi-okr/header/Header"
+import useWebSocket from "../../hook/useWebSocket.jsx";
+import {useEffect, useState} from "react";
+import {unsetToken} from "../../lib/authenticate.js";
 
 const Template = () => {
+    const navigate = useNavigate();
+    const [ user, setUser ] = useState(null);
+    const ws = useWebSocket({ handleNoToken: logout });
+
+  function logout() {
+    unsetToken();
+    setUser(null);
+    navigate('/');
+  }
+
+  useEffect(() => {
+    if (!ws.lastJsonMessage) return;
+
+    const message = ws.lastJsonMessage;
+    switch (message.type) {
+      case 'loginSuccess': {
+        const user = message.body.user;
+        setUser(user);
+        console.log(`Successfully logged in as '${user.name}'`)
+        break;
+      }
+      case 'error': {
+        if (message.body.error === 'Invalid authentication token') {
+          logout();
+        } else {
+          console.error(message.body);
+        }
+        break;
+      }
+    }
+  }, [ ws.lastJsonMessage ]);
+
     return (
         <>
             <Header/>
