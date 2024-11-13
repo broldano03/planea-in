9fb56d726f48d9f0a5f3a87dc55f8cafb1e8a737
yusshu <andreroldan.me@gmail.com>
2024-11-12 23:03:51 -0500

    *: register /login path

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import Kanban from './components/kanban/Kanban.jsx'
 import Template from './components/main/Template.jsx'
 import { ItemsProvider } from './context/ItemsContext.jsx'
+import Login from "./pages/Login.jsx";
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
                         <Route path="/cuadro-de-mando" element={<App/>} />
                         <Route path="/tablero-kanban" element={<Kanban />} />
                     </Route>
+                    <Route path="/login" element={<Login/>} />
                 </Routes>
             </ItemsProvider>
     </BrowserRouter>
